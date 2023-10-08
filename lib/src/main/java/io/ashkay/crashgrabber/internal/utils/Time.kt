121af2d8134c5ce2354bf696005e3b5b6e48f264
Ashish Kumar <akuzi770@gmail.com>
2023-10-08 21:20:05 +0530

    added am/pm to date formatting

--- lib/src/main/java/io/ashkay/crashgrabber/internal/utils/Time.kt
+++ lib/src/main/java/io/ashkay/crashgrabber/internal/utils/Time.kt
@@ -3,8 +3,8 @@ package io.ashkay.crashgrabber.internal.utils
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-const val DATE_TIME_PATTERN = "dd-M-yyyy hh:mm:ss"
-const val TIME_PATTERN = "hh:mm:ss"
+const val DATE_TIME_PATTERN = "dd-M-yyyy hh:mm:ss a"
+const val TIME_PATTERN = "hh:mm:ss a"
 
 internal fun Long.toDateTime(): String {
     return SimpleDateFormat(DATE_TIME_PATTERN, Locale.getDefault()).format(this)
